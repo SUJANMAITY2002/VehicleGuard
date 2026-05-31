96300275c8d0ab56f78f9ad2f1d062ff1b64f13e
SUJANMAITY2002 <sujanmaity917@gmail.com>
2026-06-01 00:20:21 +0530

    Update Createoutwardweighment.jsx
    edit the css name

--- frontend/src/pages/Manufacturing/Inventory/Weighment/CreateWeighment/Createoutwardweighment.jsx
+++ frontend/src/pages/Manufacturing/Inventory/Weighment/CreateWeighment/Createoutwardweighment.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./CreateOutwardWeighment.css";
+import "./Createoutwardweighment.css";
 import ModuleNavbar from "../../../../../components/ModuleNavbar/ModuleNavbar";
 
 const GIN_API = "http://localhost:5000/api/goods-inward-note";
