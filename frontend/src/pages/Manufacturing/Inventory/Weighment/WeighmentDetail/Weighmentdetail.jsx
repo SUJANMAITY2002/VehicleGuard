59d50833f71fa5aa728fb8c663fac4be708b79a8
Sujan Maity <sujanmaity917@gmail.com>
2026-06-01 00:09:36 +0530

    rename weightdetails

--- frontend/src/pages/Manufacturing/Inventory/Weighment/WeighmentDetail/Weighmentdetail.jsx
+++ frontend/src/pages/Manufacturing/Inventory/Weighment/WeighmentDetail/Weighmentdetail.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import "./WeighmentDetail.css";
